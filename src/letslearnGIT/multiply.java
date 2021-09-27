13efd203390be7986700d1d75268f33d8bff7ba3
AB <abhikushal345@gmail.com>
2021-09-27 17:13:25 +0530

    multiply.java is changed

--- src/letslearnGIT/multiply.java
+++ src/letslearnGIT/multiply.java
@@ -4,6 +4,6 @@ public class multiply {
 	 public static void main(String[] args) {
 			int a=10;
 			int b=15;
-			System.out.println("sum "+a*b);
+			System.out.println("multiplication"+a*b);
 		}
 }
